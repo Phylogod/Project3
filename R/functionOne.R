ed4664eaec3570dc906cea63a482d5c161ffa82e
Tyler Tran <tyler.tran47@yahoo.com>
2019-11-21 20:32:57 +0000

    moving files:

---
+++ R/functionOne.R
@@ -0,0 +1,22 @@
+#' Queries data from genbank.
+#' 
+#' Each object listed in the list is searched inside genbank and all availble data
+#' is converted into a dataframe containg an assortment of topics for each data input. 
+#' 
+#' 
+#' @param taxalist A list containing mulitple species and genus names you would like to query genbank for data for. 
+#' @return megataxa A dataframe of all the species or genus you searched for and their associated data.
+#' @export query_id4 
+
+query_id4 <- function(taxalist) {
+  ids <-c()
+  for (taxa in taxalist){
+    print(taxa)
+    raw <- name_suggest(taxa)
+    print(raw)
+    ids[taxa] <- c(raw$key)
+  }
+  taxa_occurrences <- lapply(ids, occ_search, return = "data")
+  mega_taxa <- bind_rows(taxa_occurrences, .id = "column_labels")
+  return(mega_taxa)
+}
